bcb5430069126a56a000c56e8d25d4ba84f4f276
panos1998 <petridisp@ece.auth.gr>
2021-04-15 15:28:39 +0300

    Helmet conf

--- app.js
+++ app.js
@@ -29,9 +29,10 @@ app.use(helmet({
   contentSecurityPolicy:{
     directives:{
       "default-src":["'self'"],
-      "style-src": ["'self'","https://stackpath.bootstrapcdn.com","sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z"],
       "script-src":["'self'","https://code.jquery.com","sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"],
+      "style-src": ["'self'","https://stackpath.bootstrapcdn.com","sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z"],
       "script-src-elem":["https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js","sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV"],
+
     },
   }
 }
