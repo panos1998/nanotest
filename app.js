fe8a0233c9c840e731d89fc1d2213264b92e8a92
panos1998 <petridisp@ece.auth.gr>
2021-04-15 16:21:58 +0300

    Helmet conf

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(helmet({
     directives:{
       "default-src":["'self'"],
       "script-src":["https://code.jquery.com/jquery-3.5.1.slim.min.js","https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js","'unsafe-inline'","'sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj'"],
-      "style-src": ["https://stackpath.bootstrapcdn.com","sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z"],
+      "style-src": ["https://stackpath.bootstrapcdn.com","'sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z'"],
       "object-src":["'none'"],
 
 
