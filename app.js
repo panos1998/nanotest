4cf19f7212d0167cab2389b7d8d4b9843205d04b
panos1998 <petridisp@ece.auth.gr>
2021-11-17 19:48:58 +0200

    IP auditing

--- app.js
+++ app.js
@@ -71,7 +71,9 @@ app.use((req,res,next)=>{
 });
 app.use('/', indexRouter);
 app.use('/catalog',catalogRouter);
-
+app.use(function (req,res,next){
+  console.log(req.ip)
+})
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
