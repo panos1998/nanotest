462b4f739358cf57204869cce75943c9ffbd9f00
panos1998 <petridisp@ece.auth.gr>
2021-11-22 21:24:40 +0200

    remove request.ip

--- app.js
+++ app.js
@@ -73,9 +73,7 @@ app.use((req,res,next)=>{
 app.use('/', indexRouter);
 app.use('/catalog',catalogRouter);
 
-app.use(function (req,res,next){
-  console.log(req.ip)
-})
+
 
 
 // catch 404 and forward to error handler
